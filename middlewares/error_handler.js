164026741a937026e722951ca30378b605cb65f5
Adrian Ruggia <ruggia.adrian@gmail.com>
2023-08-10 17:44:55 -0300

    Delete middlewares directory

--- middlewares/error_handler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (error,req,res,next) => {
-    console.log(error)
-    return res.status(error.status || 500).json({
-        statusCode: error.status,
-        messages: error.message
-    })
-}
-
-export default errorHandler
--- middlewares/not_found_handler.js
+++
@@ -1,8 +0,0 @@
-import createHttpError from "http-errors"
-
-
-const notFoundHandler = (req, res, next) => {
-    next(createHttpError(404, 'Ruta no encontrada'))
-}
-
-export default notFoundHandler
