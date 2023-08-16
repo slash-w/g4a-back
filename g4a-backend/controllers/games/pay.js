46b18558d6050981a073f0736038885ac3e3b996
Samuel García <120742240+Samuel0273@users.noreply.github.com>
2023-08-16 11:02:06 -0500

    Samuel (#5)
    * register
    
    * admin panel
    
    * pay

---
+++ g4a-backend/controllers/games/pay.js
@@ -0,0 +1,20 @@
+const stripe = require("stripe")(
+  "sk_test_51NfjIYGeX3wi2NjaFBArkYpXGhmP2Um7sRqWrxlwHvg6JNN3RE16noGDAwZrj5agKk5SMN8tHzFxfXHGmWVfIIFM00UPBTIjAB"
+);
+
+export default async (req, res, next) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: req.body.amount,
+      currency: "usdS",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+
+    res.json({ paymentIntent: paymentIntent.client_secret });
+  } catch (error) {
+    console.error("Error creating Payment Intent:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+};
--- g4a-backend/routes/games.js
+++ g4a-backend/routes/games.js
@@ -6,11 +6,15 @@ import updateGame from "../controllers/games/update.js";
 import destroyGame from "../controllers/games/destroy.js";
 import passport from "../middlewares/passport.js";
 import validator from "../middlewares/validator.js";
+import create from "../controllers/games/create.js";
+import pay from "../controllers/games/pay.js";
 
 const game_router = Router();
 
 game_router.get("/:id", read_one);
 game_router.get("/", read); //leer uno o todos
+game_router.post("/create", create); //leer uno o todos
+game_router.post("/payment", pay);
 game_router.put(
   "/:id",
   validator(gameUpdate),
